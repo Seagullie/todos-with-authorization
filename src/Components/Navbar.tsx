36f0785ec8126bf4c678777bf7169c981bf20fee
Seagullie <59409233+Seagullie@users.noreply.github.com>
2025-03-17 18:48:27 +0200

    feat: Update Navbar link to point to todo_lists page

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-xl font-bold">
-          <Link to="/">Insiders TODOs Test Project</Link>
+          <Link to="/todo_lists">Insiders TODOs Test Project</Link>
         </div>
         <div className="space-x-4">
           <Link to="/login" className="text-gray-300 hover:text-white">
